6f8da52e2e80a25c08c9eee20e9eac44dfe1f7a6
Travis CI <travis@travis-ci.org>
2021-07-12 17:03:08 +0200

    QuickSilverSearch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import './App.css'
+import Coin from './Coin';
+import QuickSilverSearch from './QuickSilverSearch';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Comment {
+  render() {
+    return (
+      <div>
+        <QuickSilverSearch></QuickSilverSearch>
+      </div>
+    )
+  }
 }
 
 export default App;
