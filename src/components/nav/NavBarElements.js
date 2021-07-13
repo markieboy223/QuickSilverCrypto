55534cd91dac86d67f730bf1c093ab8c0d7abb85
Travis CI <travis@travis-ci.org>
2021-07-13 20:17:50 +0200

    navbar fix

--- src/components/nav/NavBarElements.js
+++ src/components/nav/NavBarElements.js
@@ -3,7 +3,7 @@ import { NavLink as Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const Nav = styled.nav`
-  background: #000;
+  background-color: #1a1a1c;
   height: 80px;
   display: flex;
   justify-content: space-between;
