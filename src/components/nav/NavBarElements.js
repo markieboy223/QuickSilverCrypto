20e7dbe611fe67323a5e8f9ab46ff89851ba1b3f
Travis CI <travis@travis-ci.org>
2021-07-13 20:49:43 +0200

    NavBarElementsFix

--- src/components/nav/NavBarElements.js
+++ src/components/nav/NavBarElements.js
@@ -3,11 +3,13 @@ import { NavLink as Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const Nav = styled.nav`
-  height: 40px;
+  height: 80px;
   display: flex;
   justify-content: space-between;
   padding: 0.5rem calc((100vw - 1000px) / 2);
   z-index: 10;
+  /* Third Nav */
+  /* justify-content: flex-start; */
 `;
 
 export const NavLink = styled(Link)`
@@ -41,10 +43,11 @@ export const NavMenu = styled.div`
   display: flex;
   align-items: center;
   margin-right: -24px;
-   margin-right: 24px; 
-   Third Nav 
-   width: 100vw;
-  white-space: nowrap; 
+  /* Second Nav */
+  /* margin-right: 24px; */
+  /* Third Nav */
+  /* width: 100vw;
+  white-space: nowrap; */
   @media screen and (max-width: 768px) {
     display: none;
   }
@@ -54,6 +57,9 @@ export const NavBtn = styled.nav`
   display: flex;
   align-items: center;
   margin-right: 24px;
+  /* Third Nav */
+  /* justify-content: flex-end;
+  width: 100vw; */
   @media screen and (max-width: 768px) {
     display: none;
   }
@@ -69,6 +75,7 @@ export const NavBtnLink = styled(Link)`
   cursor: pointer;
   transition: all 0.2s ease-in-out;
   text-decoration: none;
+  /* Second Nav */
   margin-left: 24px;
   &:hover {
     transition: all 0.2s ease-in-out;
