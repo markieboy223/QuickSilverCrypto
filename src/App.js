78f62daa36cb3e700bc4db2549f881c67c324262
Travis CI <travis@travis-ci.org>
2021-07-13 20:44:52 +0200

    eerste variant af paginas klaargezet

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import QuickSilverSearch from './pages/quicksilversearch/QuickSilverSearch';
+import contact from './pages/contact';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
 
 
 function App() {
@@ -14,6 +17,10 @@ function App() {
        <Switch>
          <Route path="/home" exact component={Home} />
          <Route path="/quicksilversearch" exact component={QuickSilverSearch} />
+         <Route path="/contact-us" exact component={contact} />
+         <Route path="/sign-up" exact component={SignUp} />
+         <Route path="/signin" exact component={SignIn} />
+
        </Switch>
      </Router>
     );
--- src/components/nav/NavBarElements.js
+++ src/components/nav/NavBarElements.js
@@ -3,13 +3,11 @@ import { NavLink as Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const Nav = styled.nav`
-  height: 80px;
+  height: 40px;
   display: flex;
   justify-content: space-between;
   padding: 0.5rem calc((100vw - 1000px) / 2);
   z-index: 10;
-  /* Third Nav */
-  /* justify-content: flex-start; */
 `;
 
 export const NavLink = styled(Link)`
@@ -43,11 +41,10 @@ export const NavMenu = styled.div`
   display: flex;
   align-items: center;
   margin-right: -24px;
-  /* Second Nav */
-  /* margin-right: 24px; */
-  /* Third Nav */
-  /* width: 100vw;
-  white-space: nowrap; */
+   margin-right: 24px; 
+   Third Nav 
+   width: 100vw;
+  white-space: nowrap; 
   @media screen and (max-width: 768px) {
     display: none;
   }
@@ -57,9 +54,6 @@ export const NavBtn = styled.nav`
   display: flex;
   align-items: center;
   margin-right: 24px;
-  /* Third Nav */
-  /* justify-content: flex-end;
-  width: 100vw; */
   @media screen and (max-width: 768px) {
     display: none;
   }
@@ -75,7 +69,6 @@ export const NavBtnLink = styled(Link)`
   cursor: pointer;
   transition: all 0.2s ease-in-out;
   text-decoration: none;
-  /* Second Nav */
   margin-left: 24px;
   &:hover {
     transition: all 0.2s ease-in-out;
